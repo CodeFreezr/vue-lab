1c53fa37e39280a4aafb2e57e94b77d8974d8474
CodeFreezr <burkhardt@web.de>
2018-03-05 21:14:12 +0100

    Add Stuff

---
+++ http-project/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Fira Code', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #909090;
+  margin-top: 60px;
+}
+body {
+  background-color: #303030;
+}
+</style>
